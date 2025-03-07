e5d3762c3ca6199d7be9c4b74bd48963f4a5f802
irem-yigit <yigitiremm@gmail.com>
2025-03-07 05:54:31 +0300

    feat: add product list project codes

---
+++ product-list/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('#listProducts').click(function() {
+      $.ajax({
+        type: 'GET',
+        url: 'products.json',
+        dataType: 'json',
+        success: function(data) {
+          $('#productList').empty(); 
+          
+          $.each(data, function(index, product) {
+            let cardHtml = `
+              <div class="card">
+                <h4>${product.name}</h4>
+                <p>Price: ${product.price} $</p>
+                <a href="${product.link}" target="_blank">Detailed Information</a>
+              </div>
+            `;
+            $('#productList').append(cardHtml);
+          });
+        },
+        error: function() {
+            alert("An error occurred while loading products.");
+        }
+  
+      });
+    });
+  
+  });
+  
